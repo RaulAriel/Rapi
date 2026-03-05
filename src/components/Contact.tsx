57dc44391f233de0044ae94b24ecb2359df52851
[dyad] <raularieldiaz@gmail.com>
2026-03-05 11:58:56 +0100

    [dyad] Fixed missing 'cn' import error - wrote 1 file(s)

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -13,6 +13,7 @@ import { showSuccess, showError } from "@/utils/toast";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
+import { cn } from "@/lib/utils";
 import {
   Form,
   FormControl,
