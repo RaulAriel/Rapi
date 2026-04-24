c3df156e2138ded28afcf900edfba2e72bb90f69
[dyad] <raularieldiaz@gmail.com>
2026-04-24 08:51:23 +0200

    Updated src/lib/constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 "use client";
 
 // Reemplaza el texto entre comillas con el ID que te salió en la consola
-export const ADMIN_ID = "PEGAR_AQUI_TU_NUEVO_ID";
+export const ADMIN_ID = "a5f369b3-8bec-4e49-8878-92196a62965a";
